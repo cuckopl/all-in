1565cc297cfe53f013058fd332a291d176b42b5b
dm <cuckopl@gmail.com>
2018-01-25 19:52:11 +0100

    init

---
+++ src/main/java/com/cudzilp/Application.java
@@ -0,0 +1,14 @@
+package com.cudzilp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackageClasses = Application.class)
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
